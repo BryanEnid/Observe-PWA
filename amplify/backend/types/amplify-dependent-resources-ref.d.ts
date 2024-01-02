c37fd4bc67a5460e9e63c249cf1c830cec38944e
Bryan Enid <bryanenid97@gmail.com>
2024-01-02 12:31:14 -0500

    still working

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,15 @@
 export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "observepwaee1dcbd2": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+	auth: {
+		observepwaee1dcbd2: {
+			AppClientID: 'string';
+			AppClientIDWeb: 'string';
+			HostedUIDomain: 'string';
+			IdentityPoolId: 'string';
+			IdentityPoolName: 'string';
+			OAuthMetadata: 'string';
+			UserPoolArn: 'string';
+			UserPoolId: 'string';
+			UserPoolName: 'string';
+		};
+	};
+};
